0b61f539e30d23f2d639980c9b9664b9ea90b8ac
Adan Escamilla <adanescamilla580@gmail.com>
2021-04-08 10:12:05 -0700

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -1,6 +1,6 @@
-let state = {count: 0};
+let state; 
 
-function changeState(state, action){
+function changeState(state = { count: 0 }, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
         return {count: state.count + 1}
@@ -17,3 +17,12 @@ function dispatch(action){
 function render(){
     document.body.textContent = state.count
 }
+
+dispatch({ type: '@@INIT' }) 
+
+//switch returns whatever state was passed into the changeState
+//we call dispatch at the bottom and pass it any type that way 
+//it will render 0 on the page 
+
+//we set our initial state in our reducer
+
